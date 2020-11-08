99b276201c7dc35a3422ece6c0d0385bdd81c9f3
Bala Venkataraman <38150148+balavenkataraman123@users.noreply.github.com>
2020-11-08 17:47:43 -0500

    Add files via upload

---
+++ primefactorizer.py
@@ -0,0 +1,31 @@
+def isprime(n):
+    if n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    elif not n == 3 and n % 3 == 0:
+        return False
+    elif not n == 5 and n % 5 == 0:
+        return False
+    else:
+        for i in range(int(n ** 0.5) + 1):
+            if not i <= 1 and n % i == 0:
+                return False
+        return True
+if __name__ == "__main__":
+    print('Prime Factorizer')
+    num = int(input('enter number')) 
+    factors = []
+    while num > 1:
+        for i in range(num + 1):
+            if not i == 1 and not i == 0 and num % i == 0 and isprime(i):
+                num = int(num / i)
+                factors.append(i)
+                if num == 1:
+                    break
+    for i in factors:
+        print(i)
+    
+                
+
+                    
